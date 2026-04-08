2454a7db5c30ebc5d5a65f382dcf7c9d0a19846f
jalarapu koushik raj <160752109+JalarapuKoushikRaj@users.noreply.github.com>
2026-04-08 05:55:41 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+            return false;
+        char[] s1 = s.toCharArray();
+        char[] s2 = t.toCharArray();
+
+        for(int i=0;i<s1.length;i++)
+        {
+            for(int j=0;j<s1.length-1;j++)
+            {
+                if(s1[j]>s1[j+1])
+                {
+                    char temp = s1[j];
+                    s1[j]=s1[j+1];
+                    s1[j+1]=temp;
+                }
+            }
+        }
+         for(int i=0;i<s2.length;i++)
+        {
+            for(int j=0;j<s2.length-1;j++)
+            {
+                if(s2[j]>s2[j+1])
+                {
+                    char temp = s2[j];
+                    s2[j]=s2[j+1];
+                    s2[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<s1.length;i++)
+        {
+            if(s1[i]!=s2[i])
+            {
+                return false;
+            }
+        }
+        return true;
+        /* if(s.length() != t.length()) return false;
+        int[] count = new int[26]; 
+        for(int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+        for(int i = 0; i < t.length(); i++) {
+            count[t.charAt(i) - 'a']--;
+        }
+        for(int i = 0; i < 26; i++) {
+            if(count[i] != 0) return false;
+        }
+        return true; */
+    }
+}
